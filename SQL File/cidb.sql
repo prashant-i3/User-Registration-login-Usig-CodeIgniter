1285ddc858f432b769a725c0e08ce40b73f65a87
Prashant Pandey <103842494+prashant-i3@users.noreply.github.com>
2022-10-11 21:56:21 +0530

    Add files via upload

---
+++ SQL File/cidb.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2021 at 01:55 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cidb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `id` int(11) NOT NULL,
+  `FirstName` varchar(150) DEFAULT NULL,
+  `LastName` varchar(150) DEFAULT NULL,
+  `Email` varchar(150) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `FirstName`, `LastName`, `Email`, `Password`, `PostingDate`) VALUES
+(2, 'Anuj', 'kumar', 'ak@gmail.com', 'Test@123', '2021-06-29 23:14:15'),
+(4, 'Amit', 'Singh', 'amitsingh@gmail.com', 'Test@12345', '2021-06-30 00:06:24');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.txt
@@ -0,0 +1,24 @@
+How to run the User registration and login using CodeIgniter
+
+1.Download the zip file
+
+2.Extract the file and copy registration-ci folder
+
+3.Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+
+4.Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+5.Create a database with name cidb
+
+6.Import cidb file(given inside the zip package in SQL file folder)
+
+7.Run the script http://localhost/registration-ci
+
+
+
+User Credential
+Username: ak@gmail.com
+Password: Test@123
+or Register a new user
+
+
